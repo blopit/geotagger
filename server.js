5aa5c31028fd3bd3dc40fe96d6b3d4eff7f14028
Shren <patel.shrenil@gmail.com>
2016-09-22 17:26:17 -0400

    added subcategories

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.post("/tags", function(req, res) {
 
   newTag.author = req.body.author || null;
   newTag.category = req.body.category || 'Other';
+  newTag.subcategory = req.body.subcategory || 'Other';
   newTag.name = req.body.name || '';
   newTag.description = req.body.description || '';
   newTag.points = 0;
